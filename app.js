4a11d03627c2a4b4ecbc5bb2fcbe62d0eb9948c6
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-08-28 14:40:35 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express') //載入express套件
 const mongoose = require('mongoose') //載入mongoose套件
-const 
+const exphbs = require('express-handlebars') //載入handlebars套件
+
 const app = express() 
 
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main' , extname:'.hbs'})); //建立名稱為hbs的樣板，並傳入exphbs
+app.set('view engine','hbs') //啟用稱為hbs的樣板引擎
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
